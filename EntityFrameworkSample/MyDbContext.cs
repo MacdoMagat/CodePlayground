c3851de9e0806a04d2b11a80b0ad3fdbb43b0f9e
Marc Dominic Magat <mdmmm100@gmail.com>
2024-03-06 14:34:11 +0800

    add: Added EntityFrameworkSample project

---
+++ EntityFrameworkSample/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameworkSample.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkSample
+{
+    internal class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("MyDbContext")
+        {
+            
+        }
+        public DbSet<StudentModel> Students { get; set; }
+    }
+}
---
+++ EntityFrameworkSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using EntityFrameworkSample.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EntityFrameworkSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDbContext db = new MyDbContext();
+            db.Students.Add(new StudentModel { Name = "Marc Dominic Magat", Description = "My Description" });
+            db.SaveChanges();
+            Console.WriteLine("Saved");
+            Console.ReadLine();
+        }
+    }
+}
